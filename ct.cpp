9bc506bfe6a52ff9d8878b64185ea6f492e1db7e
Victor Taksheyev <victortaksheyev@gmail.com>
2019-02-08 14:03:44 -0800

    Add files via upload

--- ct.cpp
+++ ct.cpp
@@ -3,7 +3,28 @@
 
 using namespace std;
 
-class employee {
+class human {
+    public:
+        human();
+        human(string);
+        void printInfo();
+    private:
+        string name;
+};
+
+void human::printInfo() {
+    cout << "Name: " << name << endl;
+}
+
+human::human(string humName) {
+    name = humName;
+}
+
+human::human() {        //default constructor
+    name = "defaultName";
+}
+
+class employee : public human {
 	public:
         employee(int, string);
         void getVals(string&, int&, int&);
@@ -11,7 +32,6 @@ class employee {
         void printInfo();
         
     private:
-        string name;
         int salary;
         int id;
         static int empNum;
@@ -20,16 +40,16 @@ class employee {
 // initializing static var
 int employee::empNum = 0;
 
-employee::employee(int empSal, string empName) {
+// creating constructor for derived class requires to initialize constructor for base class
+// empName initializes both
+employee::employee(int empSal, string empName) : human(empName) {
     salary = empSal;
-    name = empName;
     id = empNum++; // give the first object defined 0, the next 1, etc...
 }
 
 // getter func
 void employee::getVals(string& empName, int& empSal, int& empId) {
     empSal = salary;
-    empName = name;
     empId = id;
 }
 
@@ -42,7 +62,7 @@ void employee::printInfo() {
     cout << endl;
     cout << "----------------------------------------------" << endl;    
     cout << "Information about employee id " << id << endl;
-    cout << "Name: " << name << endl;
+    human::printInfo();
     cout << "Salary: " << salary << endl;
     cout << "----------------------------------------------" << endl;
 }
@@ -52,9 +72,15 @@ int main() {
     int empSal;
     int empId;
     
-    employee emp1(10, "Mikian");
+    human hum1("Ilya");
+    hum1.printInfo();
+    
+    employee emp1(10, "Mikian"); // name is only applied to instance of name var
+                                 // that derived class inherits from base
+                                 // not applied to base class itself
     employee emp2(20, "Victor");
     emp1.printInfo();
+    hum1.printInfo();
     emp2.printInfo();
     
     return 0;
