398151456962de4f6f9bf07914af6698ac93e4f7
Victor Taksheyev <victortaksheyev@gmail.com>
2019-03-18 20:41:30 -0700

    Add files via upload

--- pointers/t.cpp
+++ pointers/t.cpp
@@ -1,42 +1,21 @@
 #include <iostream>
-
 using namespace std;
 
-class house {
+class a {
 public:
-    int *nums;                      // creating a pointer of type int;
+    a () {
+        *xPtr = 10;
+        x = 20;
+    }
+    int *xPtr = new int(10);
+protected:
+    int x;
+    
 };
 
-
-void printcArr(int size, char c[]) {
-    for (int i = 0; i < size; i++)
-        cout << c[i] << " ";
-    cout << endl;
+int main () {
+    a A;
+    A.*xPtr;
+    
+    return 0;
 }
-
-int main() {
-//    house *myhouse;
-//    myhouse = new house;            // allocating house amount of memory to the pointer
-//
-//    int *nums = new int[10];        // creating a dynamic array of 10 elements
-//    nums[2] = 9;
-//    myhouse->nums = nums;
-//
-//
-//    cout << myhouse->nums << endl;  // memory location of 1st element
-//    cout << *((myhouse->nums)+2) << endl;  // memory location of 1st element
-    
-    char c[10];
-    c[0] = 'H';
-    c[1] = 'e';
-    c[2] = 'y';
-    c[3] = '\0';
-    
-    printcArr(10, c);
-    
-    
-    
-    
-}
-
-
