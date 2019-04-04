5b879ec3a96abd22122c4f767f50754b0c56a633
victortaksheyev <victortaksheyev@gmail.com>
2019-04-03 21:45:34 -0700

    upload files

---
+++ c#/myApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class ReadFromFile
+{
+    // takes a word and returns its initial
+    static char initial(string s) {             // static - method that belongs to the class (don't have to create object of that class to access it)   
+                                                // Math.sqrt(for example)
+        char init;
+        init = s[0];
+        return init;
+    }
+    static void Main()
+    {
+        string phrase = "Victor";
+        int numChars = phrase.Length;              // returns # chars in string
+        initial(phrase);
+        Console.WriteLine();              
+    }
+}
