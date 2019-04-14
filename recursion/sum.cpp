cf276234bae3bae9593923cd1b3b6c79d7b454af
Victor Taksheyev <victortaksheyev@gmail.com>
2019-04-13 21:22:17 -0700

    Add files via upload

---
+++ recursion/sum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int sum(int *arr) {
+
+    if (n = 1) {
+        return arr[n];
+    }
+}
+
+int main () {
+    int *arr = new int[10];
+    for (int i = 0; i < 10; i++) {
+       arr[i] = rand()%10; 
+    }
+    for (int i = 0; i < 10; i++)
+    cout << arr[i]<< " ";
+
+
+}
